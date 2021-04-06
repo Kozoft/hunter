f31ad9f71e995f8d8aee41c21cdefddf36a6e2de
Aleksandr Baranov <aleksandr.baranov@wartsila.com>
2021-04-06 21:45:22 +0300

    Final gameplay

--- Hunter/main.cpp
+++ Hunter/main.cpp
@@ -21,19 +21,17 @@ int main(int, char const**)
     RenderWindow window(VideoMode(2000, 1000), "SLOH N MYXA");
     CircleShape oxotnik(20.f);
     CircleShape dobocha1(200.f);
-    CircleShape dobocha2(10.f, 5);
-    CircleShape pula(10.f, 5);
+    CircleShape dobocha2(300.f, 5);
+    CircleShape pula(10.f);
     CircleShape strelka(90.f, 3);
     strelka.setPosition(725, 500);
     dobocha1.setFillColor(Color::Cyan);
-    dobocha2.setFillColor(Color::Cyan);
+    dobocha2.setFillColor(Color(255, 255, 255, 128));
     oxotnik.setPosition(50, rand() % 1000);
     dobocha1.setPosition(rand() % 2000, rand() % 1000);
-    dobocha2.setPosition(rand() % 2000, rand() % 1000);
+    dobocha2.setPosition(dobocha1.getPosition().x - 90, dobocha1.getPosition().y - 90);
     c = dobocha1.getPosition().x;
     d = dobocha1.getPosition().y;
-    c2 = dobocha2.getPosition().x;
-    d2 = dobocha2.getPosition().y;
     pula.setPosition(oxotnik.getPosition().x, oxotnik.getPosition().y);
     int xp = -10;
     sf::Font font;
@@ -52,7 +50,7 @@ int main(int, char const**)
     Text text4("YOU DEAD", font, 300);
     text4.setFillColor(Color::Black);
     text4.setPosition(-1000, -1000);
-    Text pravila("e = STRELBA, w a s d = DVIZEHNE, q = OTRAZENIE PULI, SHACHALA YBEITE MYXY, POTOM CLONA", font, 30);
+    Text pravila("PROBEL = STRELBA, w a s d = DVIZEHNE, p = OTRAZENIE PULI, SHACHALA YBEITE CHIT, POTOM CLONA", font, 30);
     pravila.setFillColor(Color::Red);
     pravila.setPosition(0, 200);
     while (window.isOpen())
@@ -65,74 +63,80 @@ int main(int, char const**)
             if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
                 window.close();
             }
-            if (event.type == Event::KeyPressed && (event.key.code == Keyboard::E || event.key.code == Keyboard::Space)) {
+            if (event.type == Event::KeyPressed && (event.key.code == Keyboard::E || event.key.code == Keyboard::Space)){
                 i = true;
                 i2 = false;
                 pr = false;
             }
-            if (event.type == Event::KeyPressed && event.key.code == Keyboard::Q) {
+            if (event.type == Event::KeyPressed && event.key.code == Keyboard::P) {
                 i2 = true;
             }
-            if (event.type == Event::KeyPressed && (event.key.code == Keyboard::Down || event.key.code == Keyboard::S)) {
-                oxotnik.setPosition(oxotnik.getPosition().x, oxotnik.getPosition().y + 12);
+            if (event.type == Event::KeyPressed && (event.key.code == Keyboard::Down || event.key.code == Keyboard::S)){
+                for (int i = 0; i < 12; i++) {
+                    oxotnik.setPosition(oxotnik.getPosition().x, oxotnik.getPosition().y + 1);
+                    window.draw(oxotnik);
+                }
                 pr = false;
             }
-            if (event.type == Event::KeyPressed && (event.key.code == Keyboard::Up || event.key.code == Keyboard::W)) {
-                oxotnik.setPosition(oxotnik.getPosition().x, oxotnik.getPosition().y - 12);
+            if (event.type == Event::KeyPressed && (event.key.code == Keyboard::Up || event.key.code == Keyboard::W)){
+                for (int i = 0; i < 12; i++) {
+                    oxotnik.setPosition(oxotnik.getPosition().x, oxotnik.getPosition().y - 1);
+                    window.draw(oxotnik);
+                }
                 pr = false;
             }
-            if (event.type == Event::KeyPressed && (event.key.code == Keyboard::Left || event.key.code == Keyboard::A)) {
-                oxotnik.setPosition(oxotnik.getPosition().x - 12, oxotnik.getPosition().y);
+            if (event.type == Event::KeyPressed && (event.key.code == Keyboard::Left || event.key.code == Keyboard::A)){
+                for (int i = 0; i < 12; i++) {
+                    oxotnik.setPosition(oxotnik.getPosition().x - 1, oxotnik.getPosition().y);
+                    window.draw(oxotnik);
+                }
                 pr = false;
             }
             if (event.type == Event::KeyPressed && (event.key.code == Keyboard::Right || event.key.code == Keyboard::D)) {
-                oxotnik.setPosition(oxotnik.getPosition().x + 12, oxotnik.getPosition().y);
+                for (int i = 0; i < 12; i++) {
+                    oxotnik.setPosition(oxotnik.getPosition().x + 1, oxotnik.getPosition().y);
+                    window.draw(oxotnik);
+                }
                 pr = false;
             }
             if (!(0 <= oxotnik.getPosition().x <= window.getSize().x)){
                 oxotnik.setPosition(50, rand() % 1000);
             }
         }
-        if (c != dobocha1.getPosition().x) {
-            c = dobocha1.getPosition().x;
-        }
-        if (d != dobocha1.getPosition().y) {
-            d = dobocha1.getPosition().y;
-        }
-        if (c2 != dobocha2.getPosition().x) {
-            c2 = dobocha2.getPosition().x;
-        }
-        if (d2 != dobocha2.getPosition().y) {
-            d2 = dobocha2.getPosition().y;
-        }
+        c = dobocha1.getPosition().x;
+        d = dobocha1.getPosition().y;
+        c2 = dobocha2.getPosition().x;
+        d2 = dobocha2.getPosition().y;
         oxotnik.setFillColor(Color(rand() % 255, rand() % 255, rand() % 255));
-        if (pula.getGlobalBounds().intersects(dobocha1.getGlobalBounds())) {
-            if (x2 <= 0) {
+        if (x2 > 0) {
+            dobocha2.setPosition(dobocha1.getPosition().x - 90, dobocha1.getPosition().y - 90);
+        }
+        if (pula.getGlobalBounds().intersects(dobocha1.getGlobalBounds()) || pula.getGlobalBounds().intersects(dobocha2.getGlobalBounds())) {
+            if (pula.getGlobalBounds().intersects(dobocha1.getGlobalBounds())) {
                 x1 -= 1;
+            } else {
+                x2 -= 1;
             }
             a = oxotnik.getPosition().x;
             b = oxotnik.getPosition().y;
-            X = (a - dobocha1.getPosition().x) / 7000;
-            Y = (b - dobocha1.getPosition().y) / 7000;
+            if (x2 > 0) {
+                X = (a - dobocha1.getPosition().x) / 4000;
+                Y = (b - dobocha1.getPosition().y) / 4000;
+            } else {
+                X = (a - dobocha1.getPosition().x) / 8000;
+                Y = (b - dobocha1.getPosition().y) / 8000;
+            }
+            
             in = false;
             i = false;
             i2 = false;
             dobocha1.setFillColor(Color(rand() % 255, rand() % 255, rand() % 255));
         }
-        if (pula.getGlobalBounds().intersects(dobocha2.getGlobalBounds()) || dobocha2.getGlobalBounds().intersects(dobocha1.getGlobalBounds())) {
-            x2 -= 1;
-            X2 = 0;
-            Y2 = 0;
-            a2 = 0;
-            b2 = 0;
-            a2 = oxotnik.getPosition().x + 25;
-            b2 = oxotnik.getPosition().y;
-            X2 = (a2 - dobocha1.getPosition().x) / 1000;
-            Y2 = (b2 - dobocha1.getPosition().y) / 1000;
+        if (pula.getGlobalBounds().intersects(dobocha2.getGlobalBounds())) {
             in = false;
             i = false;
             i2 = false;
-            dobocha2.setFillColor(Color(rand() % 255, rand() % 255, rand() % 255));
+            dobocha2.setFillColor(Color(rand() % 255, rand() % 255, rand() % 255, 128));
         }
         if (oxotnik.getGlobalBounds().intersects(dobocha1.getGlobalBounds()) || oxotnik.getGlobalBounds().intersects(dobocha2.getGlobalBounds())) {
             oxotnik.setPosition(10000, 10000);
@@ -168,51 +172,25 @@ int main(int, char const**)
             xp = oxotnik.getPosition().x * 10;
             pula.setPosition(-1000, oxotnik.getPosition().y);
         }
-        if (x2 <= 0) {
-            if (c != a && dead == false) {
-                dobocha1.setPosition(c + X, d);
-                c = dobocha1.getPosition().x;
-            } else if (dead == false) {
-                X = 0;
-                Y = 0;
-            }
-            if (d != b && dead == false) {
-                dobocha1.setPosition(c, d + Y);
-                d = dobocha1.getPosition().y;
-            } else if (dead == false) {
-                X = 0;
-                Y = 0;
-            }
-            if (((X < 0 && c < a) || (X > 0 && c > a)) && dead == false){
-                c = a;
-                X = 0;
-            }
-            if (((Y < 0 && d < b) || (Y > 0 && d > b)) && dead == false){
-                d = b;
-                Y = 0;
-            }
-        }
-        if (c2 != a2 && dead == false) {
-            dobocha2.setPosition(c2 + X2, d2);
-            c2 = dobocha2.getPosition().x;
+        if (c != a && dead == false) {
+            dobocha1.setPosition(c + X, d);
+            c = dobocha1.getPosition().x;
         } else if (dead == false) {
-            X2 = 0;
-            Y2 = 0;
+                X = 0;
         }
-        if (d2 != b2 && dead == false) {
-            dobocha2.setPosition(c2, d2 + Y2);
+        if (d != b && dead == false) {
+            dobocha1.setPosition(c, d + Y);
             d = dobocha1.getPosition().y;
         } else if (dead == false) {
-            X2 = 0;
-            Y2 = 0;
+            Y = 0;
         }
-        if (((X2 < 0 && c2 < a2) || (X2 > 0 && c2 > a2)) && dead == false){
-            c2 = a2;
-            X2 = 0;
+        if (((X < 0 && c < a) || (X > 0 && c > a)) && dead == false){
+            c = a;
+            X = 0;
         }
-        if (((Y2 < 0 && d2 < b2) || (Y2 > 0 && d2 > b2)) && dead == false){
-            d2 = b2;
-            Y2 = 0;
+        if (((Y < 0 && d < b) || (Y > 0 && d > b)) && dead == false){
+            d = b;
+            Y = 0;
         }
         window.clear();
         window.draw(oxotnik);
